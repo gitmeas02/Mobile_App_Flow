ddfea0920f9afa562fd0f5343ed53bdf5e76a342
PhengMenghour <phengmenghour@gmail.com>
2025-05-25 16:18:46 +0700

    feat(search): complete search bar with UI and functionality

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: "SF Pro Display",
+        scaffoldBackgroundColor: Colors.white,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
       home: const HomeScreen(),
     );
