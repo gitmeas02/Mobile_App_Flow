8183bad551b596f2b22338fa5f9739469193b0ab
PhengMenghour <phengmenghour@gmail.com>
2025-06-03 14:28:19 +0700

    feat(ui): initialLoginScreen, logInScreen, SignUpScreen are done

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/screens/home_screen.dart';
+import 'package:my_project/screens/initial_login_screen.dart';
 import 'package:my_project/screens/login_screen.dart';
 import 'package:my_project/screens/onboarding_screen.dart';
+import 'package:my_project/screens/signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
-      home: const LoginScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
