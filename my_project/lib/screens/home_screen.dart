47f5dcd5d038fa73b9e3b5db768e102e75a10d48
gitmeas02 <measreaksmey02@gmail.com>
2025-06-03 17:58:06 +0700

    merge feature/auth-onboarding-flow to main

--- my_project/lib/screens/home_screen.dart
+++ my_project/lib/screens/home_screen.dart
@@ -9,10 +9,7 @@ class HomeScreen extends StatefulWidget {
      @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-  class _HomeScreenState extends State<HomeScreen> {
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _searchController = TextEditingController();
