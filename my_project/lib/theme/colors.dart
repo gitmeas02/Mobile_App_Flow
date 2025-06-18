03ada1d1be717762f9e93700783ba1868214837c
gitmeas02 <measreaksmey02@gmail.com>
2025-06-19 05:53:27 +0700

    delete every thing in lib

--- my_project/lib/theme/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppPallete {
-  static const Color backgroundColor1 = Color(0xFF53B175);
-  static const Color backgroundColor2 = Color(0xFFFFFFFF);
-  static const Color primaryColor = Color(0xFF6200EE);
-  static const Color secondaryColor = Color(0xFF03DAC6);
-  static const Color errorColor = Color(0xFFB00020);
-}
